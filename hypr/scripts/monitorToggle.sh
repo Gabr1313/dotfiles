8f5d17e7e7b5f182a2d7a5381f661ae8464d23bf
Gabr1313 <gabriele.baietta03@gmail.com>
2023-01-27 12:10:07 +0100

    Changed a script and some binds, so they are more intuitive

--- hypr/scripts/monitorToggle.sh
+++ hypr/scripts/monitorToggle.sh
@@ -1,8 +1,16 @@
 OUTPUT=`hyprctl monitors`
+WORKSPACE=`hyprctl activewindow -j | jq -r '.workspace.id'`
 
-if [[ "$OUTPUT" == *"eDP-1"* ]]
-  then
-    hyprctl keyword monitor "eDP-1,disable"
-  else
-    hyprctl keyword monitor "eDP-1,1920x1080,0x0,1"
+if [[ "$OUTPUT" == *"DP-2"* ]]
+then 
+  if [[ "$OUTPUT" == *"eDP-1"* ]]
+    then
+      hyprctl keyword monitor "eDP-1,disable"
+    else
+      hyprctl keyword monitor "eDP-1,1920x1080,0x0,1"
+  fi
+elif [[ "$OUTPUT" == *"eDP-1"* ]]
+then
+  $HOME/.config/hypr/scripts/lock
 fi
+hyprctl dispatch workspace $WORKSPACE
--- hypr/scripts/monitorToggleHypr.sh
+++
@@ -1,8 +0,0 @@
-OUTPUT=`hyprctl monitors`
-
-if [[ "$OUTPUT" == *"eDP-1"* ]]
-  then
-    hyprctl keyword monitor "eDP-1,disable"
-  else
-    hyprctl keyword monitor "eDP-1,1920x1080,0x0,1"
-fi
