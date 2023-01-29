7ff6490e478f352ff334e5129df900769b943b04
Gabr1313 <gabriele.baietta03@gmail.com>
2023-01-29 22:49:03 +0100

    Improved some scripts & modified clock settings

--- hypr/scripts/screenFullHypr.sh
+++ hypr/scripts/screenFullHypr.sh
@@ -1,6 +1,4 @@
-TIME=`timedatectl | grep Local | sed 's/^.\{,31\}//' | sed 's/.\{,4\}$//' \
-  | sed 's/\-//g' | sed 's/\://g' | sed 's/ /\-/'`
-SSPATH=~/screenshots/screenshot-$TIME.png
+SSPATH=~/screenshots/screenshot-$(date +%y%m%d-%H%M%S).png
 
 grim -o $(hyprctl monitors -j | jq -r '.[] | select(.focused) | .name') $SSPATH
 xclip -selection clipboard -t image/png -i $SSPATH
--- hypr/scripts/screenWindowHypr.sh
+++ hypr/scripts/screenWindowHypr.sh
@@ -1,6 +1,4 @@
-TIME=`timedatectl | grep Local | sed 's/^.\{,31\}//' | sed 's/.\{,4\}$//' \
-  | sed 's/\-//g' | sed 's/\://g' | sed 's/ /\-/'`
-SSPATH=~/screenshots/screenshot-$TIME.png
+SSPATH=~/screenshots/screenshot-$(date +%y%m%d-%H%M%S).png
 
 grim -g "$(hyprctl clients -j | jq -r ".[] | select(.workspace.id == "$(hyprctl activewindow -j | jq -r '.workspace.id')\)""| jq -r ".at,.size" | jq -s "add" | jq '_nwise(4)' | jq -r '"\(.[0]),\(.[1]) \(.[2])x\(.[3])"'| slurp)" "$SSPATH"
 xclip -selection clipboard -t image/png -i $SSPATH
