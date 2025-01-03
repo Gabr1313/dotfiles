e7851b94ab0e440ba2ebe730c5b897bcdcc74537
Gabr1313 <gabriele.baietta03@gmail.com>
2025-01-03 22:57:58 +0100

    updates

--- fish/config.fish
+++ fish/config.fish
@@ -4,18 +4,26 @@ if status is-interactive
 	set fish_color_command green
 
 	set -g fish_key_bindings fish_vi_key_bindings
-	bind --mode insert  \cb prevd-or-barkward-word
+	# previous directory
 	bind --mode default \cb prevd-or-backward-word
+	bind --mode insert  \cb prevd-or-barkward-word
 	bind --mode visual  \cb prevd-or-backward-word
-	bind --mode insert  \cf nextd-or-forward-word
+	# forward directory
 	bind --mode default \cf nextd-or-forward-word
+	bind --mode insert  \cf nextd-or-forward-word
 	bind --mode visual  \cf nextd-or-forward-word
+	# parent directory
+	bind --mode default \co 'cd .. ; commandline -f repaint'
+	bind --mode insert  \co 'cd .. ; commandline -f repaint'
+	bind --mode visual  \co 'cd .. ; commandline -f repaint'
+	# ctrl-n as tab
+	bind --mode default \cn complete
 	bind --mode insert  \cn complete
 	bind --mode visual  \cn complete
-	bind --mode default \cn complete
+	# ctrl-p as shift-tab
+	bind --mode default \cp complete-and-search
 	bind --mode insert  \cp complete-and-search
 	bind --mode visual  \cp complete-and-search
-	bind --mode default \cp complete-and-search
 
 	alias v='nvim'
 
